15d4be4553bc4748107b3d07fe7e0fb554a129cd
Nicolas Moina <nicolawsmoina@gmail.com>
2025-10-05 17:10:19 -0500

    feat: Add @vercel/speed-insights dependency and integrate SpeedInsights component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'highlight.js/styles/atom-one-dark.css';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -98,6 +100,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
